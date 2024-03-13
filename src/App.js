a2e0233552d9e76c091386a7022c6dc680fd2c65
Rebeca Monis <rebecamonis@Rebecas-Laptop.local>
2024-03-13 21:36:08 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import 'react-chatbot-kit/build/main.css'
 
+import ChatbotComponent from './components/ChatbotComponent'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ChatbotComponent/>
+       
       </header>
     </div>
   );
